d00a470bfc30c53399275c65df18693c2588d294
Shota Shimazu <hornet.live.mf@gmail.com>
2018-10-22 17:20:25 +0900

    add deep link application

--- DeepLinkRouter/AppDelegateExtension.swift
+++ DeepLinkRouter/AppDelegateExtension.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 
-public protocol DeepLinkRouter: class {
-    var router: Router { get set }
+
+public protocol DeepLinkRouter: class { }
+
+
+struct AssociatedKey {
+    static var routerKey: Router? = nil
 }
+
 
 extension DeepLinkRouter where Self: UIApplicationDelegate {
     
+    private(set) var router: Router {
+        get {
+            if (objc_getAssociatedObject(self, &AssociatedKey.routerKey) as? Router) != nil {
+                return self.router
+            }
+            
+            let initial = Router(url: URL(string: "default://root")!)
+            objc_setAssociatedObject(self, &AssociatedKey.routerKey, initial, .OBJC_ASSOCIATION_RETAIN)
+            
+            return initial
+        }
+        
+        set(newValue) {
+            objc_setAssociatedObject(self, &AssociatedKey.routerKey, newValue, .OBJC_ASSOCIATION_RETAIN)
+        }
+    }
+
     
     func application(_ app: UIApplication,
                      open url: URL,
---
+++ DeepLinkRouter/Objc/DeepLinkRouter-Swift.h
@@ -0,0 +1,8 @@
+//
+//  DeepLinkRouter-Swift.h
+//  DeepLinkRouter
+//
+//  Created by Shota Shimazu on 2018/10/22.
+//  Copyright © 2018 Shota Shimazu. All rights reserved.
+//
+
