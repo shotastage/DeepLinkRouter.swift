33e53479d3cafc8caadf339a85f55981e8252c08
Shota Shimazu <hornet.live.mf@gmail.com>
2018-10-22 18:21:01 +0900

    revise

--- DeepLinkRouter/Router.swift
+++ DeepLinkRouter/Router.swift
@@ -14,7 +14,7 @@ open class Router {
     var host: String?
     var path: String?
 
-    public init(url: URL) {
+    public init(from: URL) {
         self.host = url.host as String?
         self.path = url.path as String?
     }
