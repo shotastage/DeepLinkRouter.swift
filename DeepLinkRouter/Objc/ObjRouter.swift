c30431c25267f868c6accd8d9238fab2f57a06f4
Shota Shimazu <hornet.live.mf@gmail.com>
2018-10-29 00:56:27 +0900

    add experimental objc router

--- DeepLinkRouter/Objc/ObjRouter.swift
+++ DeepLinkRouter/Objc/ObjRouter.swift
@@ -14,20 +14,40 @@ import Foundation
 open class DLRouter: NSObject {
     
     var host: String
+    
     var path: String
+    
+    var pathStack: [String]
+    
+    var handlerStack: [() -> Void]
     
     public init(from: URL) {
         self.host = from.host!
         self.path = from.path
+        self.pathStack = []
+        self.handlerStack = []
     }
     
-    public func host(_ host: String, _ path: Path...) {
+    @objc
+    public func register(path: String, _ f: @escaping () -> Void) -> DLRouter {
         
-        if String(describing: self.host) == host {
-            for f in path {
-                if self.path == "/\(String(describing: f.path))" {
-                    //f.execute()
-                }
+        /// Add path to stack
+        self.pathStack.append(path)
+        
+        /// Add handler to stack
+        self.handlerStack.append(f)
+        return self
+    }
+    
+    @objc
+    public func performRouter() {
+        
+        let givenLink: String = "\(host)\(path)"
+        
+        for (index, path) in pathStack.enumerated() {
+            
+            if path == givenLink {
+                handlerStack[index]()
             }
         }
     }
