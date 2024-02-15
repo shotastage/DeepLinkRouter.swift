d9df06d31ef83621e45f7597b8b9cc64fca4ed68
shotastage <shota.shimazu@magicalsoft.app>
2024-02-15 10:30:20 +0900

    fix swift package entry

--- Package.swift
+++ Package.swift
@@ -21,11 +21,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DeepLinkRouter",
-            dependencies: []
+            dependencies: [],
+            path: "DeepLinkRouter"
         ),
         .testTarget(
             name: "DeepLinkRouterTests",
-            dependencies: ["DeepLinkRouter"]
+            dependencies: ["DeepLinkRouter"],
+            path: "DeepLinkRouterTests"
         ),
     ]
 )
